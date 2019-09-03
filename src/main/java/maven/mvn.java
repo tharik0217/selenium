6bf55d5ae3a2ed5a8a5b2901c8e868a2ee6580de
tharikthaslim <zakirhussaindarik@gmail.com>
2019-09-03 11:13:46 +0530

    maven

---
+++ src/main/java/maven/mvn.java
@@ -0,0 +1,12 @@
+package maven;
+
+public class mvn {
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\THARIK THASLIM\\eclipse-workspace\\selenium\\Driver\\chromedriver.exe");
+		
+	
+		
+	
+	}
+
+}
